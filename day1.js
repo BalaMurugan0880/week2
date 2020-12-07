e786acc7ec2a3af4cfd5716b6948ecad1d8affb9
BalaMurugan0880 <72009358+BalaMurugan0880@users.noreply.github.com>
2020-12-07 21:56:23 +0800

    Update day1.js

--- day1.js
+++ day1.js
@@ -3,3 +3,15 @@ var multiplier = 4;
 for (var i = 1; i < lines; i++){
   console.log(i+" x "+multiplier+ " = "+ i*multiplier)
  }
+
+for (var i=1; i <= 100; i++)
+{
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(i);
+}
